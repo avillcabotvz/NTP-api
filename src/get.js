62344c9a14d22be4575532b6e5b00ac0d7d2307e
Dino Pejaković <dino.pejakovic@voxdiversa.hr>
2021-08-21 00:14:52 +0200

    Task search

--- src/get.js
+++ src/get.js
@@ -1,12 +1,36 @@
 const pool = require('./dbConnection');
 
 const getTasks = (request, response) => {
-  pool.query('SELECT tasks.id,taskname,taskdesc,startdate,enddate,categoryid,statusid,personid,status.status,categories.category,person.firstname,person.lastname FROM tasks INNER JOIN status ON statusid=status.id INNER JOIN categories ON categoryid=categories.id INNER JOIN person ON personid=person.id', (error, results) => {
-    if (error) {
-      throw error
-    }
+  let params = [];
+  let where = '';
+  if (request.query.search) {
+    const searchStr = request.query.search.trim();
+    params = [`%${searchStr}%`];
+    where = 'WHERE taskname ILIKE $1';
+  }
+
+  const sql = `
+    SELECT tasks.id,
+      taskname,
+      taskdesc,
+      startdate,
+      enddate,
+      categoryid,
+      statusid,
+      personid,
+      status.status,
+      categories.category,
+      person.firstname,
+      person.lastname
+    FROM tasks
+      INNER JOIN status ON statusid = status.id
+      INNER JOIN categories ON categoryid = categories.id
+      INNER JOIN person ON personid = person.id
+    ${where}`;
+
+  pool.query(sql, params).then((results) => {
     response.status(200).json(results.rows)
-  })
+  });
 }
 
 const getCategories = (request, response) => {
